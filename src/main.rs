9315bca7d4bb539af845fa3db1f3cbdba66d6450
Stenstromen <kontakt@filipstenstrom.se>
2025-05-12 11:42:53 +0200

    :sparkles: feat: Added command line handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::path::Path;
+
+use clap::{ CommandFactory, Parser };
 use env_logger::Builder;
 use log::LevelFilter;
 
@@ -6,6 +9,15 @@ mod env;
 mod resolve;
 mod http;
 mod workflow;
+
+/// A tool for running REST workflows
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// The workflow file to execute
+    #[arg(value_name = "FILENAME")]
+    workflow_file: Option<String>,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -16,6 +28,14 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
     builder.format_timestamp_millis().init();
 
-    workflow::execute_workflow("workflow.yml").await?;
+    let args = Args::parse();
+
+    let workflow_file = args.workflow_file.unwrap_or_else(|| "workflow.yaml".to_string());
+    if !Path::new(&workflow_file).exists() {
+        println!("{}", Args::command().render_help());
+        return Err(format!("Workflow file '{}' (default) not found", workflow_file).into());
+    }
+    workflow::execute_workflow(&workflow_file).await?;
+
     Ok(())
 }
